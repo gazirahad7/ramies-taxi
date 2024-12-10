1b440b36fca81f2881aa4f67ad90dd3b82ec3dca
jamanul <jk.skb.25@gmail.com>
2024-12-11 01:57:39 +0600

    another section section done

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ let isMenuOpen = false;
 toggleButton.addEventListener("click", () => {
   if (!isMenuOpen) {
     // Open the menu
-    section.style.display = "block";
+    section.style.cssText = "display: block !important;";
     setTimeout(() => {
       section.style.opacity = 1;
     }, 10);
@@ -49,3 +49,23 @@ function hide() {
   if (window.innerWidth > 767) {
     hide(); 
   }
+
+
+  const btn = document.querySelector('.button-primary'); 
+  //console.log(btn)
+  btn.addEventListener('mousemove', function(e) {
+    const rect = btn.getBoundingClientRect();
+    const x = e.clientX - rect.left; 
+    const y = e.clientY - rect.top;  
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+  });
+  // const btnOne = document.querySelector('.button-secondary'); 
+  // //console.log(btn)
+  // btnOne.addEventListener('mousemove', function(e) {
+  //   const rect = btnOne.getBoundingClientRect();
+  //   const x = e.clientX - rect.left; 
+  //   const y = e.clientY - rect.top;  
+  //   btnOne.style.setProperty('--x', x + 'px');
+  //   btnOne.style.setProperty('--y', y + 'px');
+  // });
